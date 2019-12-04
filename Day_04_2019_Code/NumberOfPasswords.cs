6156e7dac5fa96b91ce4e756a6040e9e7ad0eafe
martinlevington <martin.levington@arup.com>
2019-12-04 13:52:03 +0000

    Day 04 Part 2

--- Day_04_2019_Code/NumberOfPasswords.cs
+++ Day_04_2019_Code/NumberOfPasswords.cs
@@ -1,26 +1,10 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices.ComTypes;
 
 namespace Day_04_2019_Code
 {
     public class NumberOfPasswords
     {
-        private readonly List<string> searchItems = new List<string>()
-        {
-            "11",
-            "22",
-            "33",
-            "44",
-            "55",
-            "66",
-            "77",
-            "88",
-            "99"
-        };
-
         public int FindPasswords(int start, int end)
         {
             var result = 0;
@@ -30,31 +14,24 @@ namespace Day_04_2019_Code
                 var passwordToCheck = i.ToString();
 
                 if (passwordToCheck.Length == 6
-                    && searchItems.Any(x => passwordToCheck.Contains(x))
+                    && passwordToCheck.Select(c => c).GroupBy(x => x).Count(y => y.Count() == 2) >= 1
                     && IsAscending(passwordToCheck)
                 )
-                {
                     result++;
-                }
-                
             }
 
 
             return result;
-
         }
 
-        public static bool IsAscending(string x)
+        private static bool IsAscending(string x)
         {
-            if (x.Length == 1)
-            {
-                return true;
-            }
+            if (x.Length == 1) return true;
 
             var last = Convert.ToInt32(x.Last().ToString());
             var prev = Convert.ToInt32(x[x.Length - 2].ToString());
 
-            return last >= prev  && IsAscending(x.Substring(0, x.Length - 1));
+            return last >= prev && IsAscending(x.Substring(0, x.Length - 1));
         }
     }
 }
--- Day_04_2019_Tests/NumberOfPasswordsTests.cs
+++ Day_04_2019_Tests/NumberOfPasswordsTests.cs
@@ -7,10 +7,14 @@ namespace Day_04_2019_Tests
     public class NumberOfPasswordsTests
     {
         [Theory]
-        [InlineData(111111, 111111, 1)]
-        [InlineData(111111, 111112, 2)]
+        [InlineData(111111, 111111, 0)]
+        [InlineData(111111, 111112, 0)]
+        [InlineData(111111, 111122, 1)]
         [InlineData(223450, 223450, 0)]
         [InlineData(123789, 123789, 0)]
+        [InlineData(112233, 112233, 1)]
+        [InlineData(123444, 123444, 0)]
+        [InlineData(111122, 111122, 1)]
         public void Test_IsAccepted(int start, int end, int expectedAnswer)
         {
             // Arrange
